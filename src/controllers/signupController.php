4b3ccbf0c0d6ff8bc20d74569c0e85e1e6e6ae74
ismail baoud <ibaoud46@gmail.com>
2025-01-14 22:32:54 +0100

    ajouter la fonction de 'signup' pour fait le traitmant de l'insersion des donne des utilisateurs dans la base de donnee

--- src/controllers/signupController.php
+++ src/controllers/signupController.php
@@ -1,7 +1,54 @@
 <?php
-class signupController{
-    public function signup(){
-        include_once  "src/views/connection/signup_view.php";
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+class signupController
+{
+    private $db;
+
+    public function connect()
+    {
+        $connection = new DatabaseConnection();
+        $this->db = $connection->connect();
+    }
+
+    public function signup()
+    {
+        echo "hi";
+        $this->connect();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $confirmPassword = $_POST['confirmPassword'];
+            $phoneNumber = $_POST['phoneNumber'];
+            $role = $_POST['role'];
+            if ($password != $confirmPassword) {
+                $err = "Passwords do not match";
+                return;
+            }
+            if ($firstName == "" || $lastName == "" || $email == "" || $password == "" || $confirmPassword == "" || $phoneNumber == "") {
+                $err = "Please fill all the fields";
+                return;
+
+            }
+            if ($role == "Student") {
+                $status = "active";
+                $student = new User($firstName, $lastName, $email, $password, $phoneNumber, $role, $status);
+                $student->signUp($this->db);
+            } elseif ($role == "teacher") {
+                $status = "On hold";
+                $student = new User($firstName, $lastName, $email, $password, $phoneNumber, $role, $status);
+                $student->signUp($this->db);
+            } else {
+                $err = "this role is not found";
+                return;
+            }
+
+        }
     }
 }
+
+//$obj = new signupController();
+//$obj->signup();
 ?>
