60bfceb53082e0781c161e054ecb5b58885f1a30
ismail baoud <ibaoud46@gmail.com>
2025-01-14 20:11:20 +0100

    ajout le classe de connection avec database apres l'ajout des tableaux dans la database

--- config/databaseConnection.php
+++ config/databaseConnection.php
@@ -1,1 +1,27 @@
 <?php
+
+
+class DatabaseConnection {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "baoud";
+
+    private $dbName = "youdemy";
+    private $conn;
+
+    public function connect()
+    {
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->dbName,
+                $this->user,
+                $this->pass
+            );
+            return $this->conn;
+
+        } catch (PDOException $e) {
+            die("Connection Error: " . $e->getMessage());
+        }
+    }
+}
+
