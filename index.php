f447882d35ef0fc8c70659d2b35a8b7a8528bef3
ismail baoud <ibaoud46@gmail.com>
2025-01-14 15:25:00 +0100

    definer les chemins des pages entre la page index pour utiliser dans le routage et definer la fonction de dispatch et sa paramerte pour fait le traitmant de routage dans le classe 'Router'

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
-include_once("src/views/home/index.php");
+<?php
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+require_once 'src/controllers/Router.php';
+require_once 'src/controllers/loginController.php';
+require_once 'src/controllers/signupController.php';
+require_once 'src/controllers/studentController.php';
+require_once 'src/controllers/teacherController.php';
+require_once 'src/controllers/adminController.php';
+require_once 'src/controllers/pathsList.php';
+
+$controllerRouter->dispatch($_SERVER['REQUEST_URI']);
 ?>
