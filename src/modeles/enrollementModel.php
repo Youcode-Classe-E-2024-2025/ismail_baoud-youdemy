f15c040a3cb6d3b675f924af51a64f9803783958
ismail baoud <ibaoud46@gmail.com>
2025-01-14 22:44:22 +0100

    definer les fonction de class enrollement et sa attributs

---
+++ src/modeles/enrollementModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class znrollement {
+    private $date;
+
+    public function __construct($date) {
+        $this->date = $date;
+    }
+
+    public function createEnrollment() {
+
+    }
+
+    public function deleteEnrollment() {
+
+    }
+
+    public function enrollmentList() {
+
+    }
+}
