0a503368fd5bac208faab1667c55f2905699c286
ismail baoud <ibaoud46@gmail.com>
2025-01-14 22:43:57 +0100

    definer les fonction de class category et sa attributs

---
+++ src/modeles/categoryModel.php
@@ -0,0 +1,23 @@
+
+<?php
+
+class category {
+    private $categoryName;
+
+    public function __construct($categoryName) {
+        $this->categoryName = $categoryName;
+    }
+
+    public function categoryInsert() {
+
+    }
+    public function categoryDelete() {
+
+    }
+    public function categoryList() {
+
+    }
+
+}
+
+?>
