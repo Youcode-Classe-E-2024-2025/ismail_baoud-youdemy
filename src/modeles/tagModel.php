a2aa85d37a4570306d3aabe34ce73a27e2610fad
ismail baoud <ibaoud46@gmail.com>
2025-01-14 22:43:13 +0100

    definer les fonction de class tags et sa attributs

---
+++ src/modeles/tagModel.php
@@ -0,0 +1,22 @@
+<?php
+
+class tag {
+    private $tagName;
+
+    public function __construct($tagName) {
+        $this->tagName = $tagName;
+    }
+
+    public function tagInsert() {
+
+    }
+    public function tagDelete() {
+
+    }
+    public function tagList() {
+
+    }
+
+}
+
+?>
