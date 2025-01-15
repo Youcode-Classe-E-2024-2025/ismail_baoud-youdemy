376f542a4d42366b4d3f895645d8cd52a5fbdf51
ismail baoud <ibaoud46@gmail.com>
2025-01-16 00:51:45 +0100

    ajouter le class enrollement et definer les attributs et constrecture et les gitters et sitters

---
+++ src/classes/enrollement.php
@@ -0,0 +1,18 @@
+<?php
+namespace src\classes;
+
+class enrollement {
+    private $date;
+
+    public function __construct($date) {
+        $this->date = $date;
+    }
+
+    public function __get($name){
+        return $this->$name;
+    }
+
+    public  function __set($name, $value){
+        $this->$name = $value;
+    }
+}
