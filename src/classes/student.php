ff722919a9c0b6245b7e17ded539404545a652db
ismail baoud <ibaoud46@gmail.com>
2025-01-16 00:52:06 +0100

    ajouter le class student et definer les attributs et constrecture et les gitters et sitters

---
+++ src/classes/student.php
@@ -0,0 +1,20 @@
+<?php
+namespace src\classes;
+
+class student extends User {
+    public function __construct($firstName, $lastName, $email, $password, $phoneNumber, $role)
+    {
+        parent::__construct($firstName, $lastName, $email, $password, $phoneNumber, $role);
+    }
+
+    public function __get($name){
+        return $this->$name;
+    }
+
+    public  function __set($name, $value){
+        $this->$name = $value;
+    }
+}
+
+
+?>
