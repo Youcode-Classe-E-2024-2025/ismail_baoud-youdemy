833dd9cd0acdc9f3b6a100a5d72c1f46af077b51
ismail baoud <ibaoud46@gmail.com>
2025-01-16 00:57:50 +0100

    ajouter les name space et includes le connection avec la base de donnee dans le classe courses

--- src/modeles/coursesModel.php
+++ src/modeles/coursesModel.php
@@ -1,23 +1,15 @@
 <?php
+namespace src\modeles;
 
-class course {
-    private $title;
-    private $description;
+use PDO;
+use config\DatabaseConnection;
+use src\classes\course;
 
-    private $content;
-    private $tagId;
 
-    private $categoryId;
-
-    private $date;
-
-    function __construct($title, $description, $content, $tagId, $categoryId, $date) {
-        $this->title = $title;
-        $this->description = $description;
-        $this->content = $content;
-        $this->tagId = $tagId;
-        $this->categoryId = $categoryId;
-        $this->date = $date;
+class courseModel {
+    private $db;
+    public function __construct(){
+        $this->db = DatabaseConnection::connect();
     }
 
     public function courseAdd(){
