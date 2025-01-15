28d3617b47e916a22b7baa59fab88ff3375d4fe4
ismail baoud <ibaoud46@gmail.com>
2025-01-16 00:59:25 +0100

    modifer la logique de connection

--- src/controllers/loginController.php
+++ src/controllers/loginController.php
@@ -1,7 +1,59 @@
 <?php
+namespace src\controllers;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+use src\classes\User;
+use src\modeles\userModel;
+
 class loginController{
-    public function login(){
-        include_once  "src/views/connection/signup_view.php";
+    public function logout(){
+        session_destroy();
+        include_once  "src/views/connection/login_view.php";
+    } 
+    public function login_controller(){
+        $status;
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $email = htmlspecialchars($_POST['email']);
+            $password = trim($_POST['password']);
+            if(!isset($email) && !isset($password)){
+                $err = "data is empty";
+                return;
+            }
+            $obg = new userModel();
+            $result = $obg->login($email, $password);
+            if($result){
+                
+                if(password_verify( $password, $result['password'] )){
+                    if($result["role"] == "student"){
+                        
+                        $_SESSION["role"] = "student";
+                        require_once "src/views/student/dashboard_view.php";
+                    }
+                    elseif($result["role"] == "teacher"){
+                        if($result["status"] == "On hold"){
+                            $_SESSION["role"] = "teacher";
+                            $_SESSION["status"] = "On hold";
+                            require_once "src/views/teacher/panding_view.php";
+                        }elseif($result["status"] == "active"){
+                            $_SESSION["role"] = "teacher";
+                            $_SESSION["status"] = "active";
+                            require_once "src/views/teacher/dashboard_view.php";
+                        }
+                    }else{
+                        $_SESSION["role"] = "admin";
+                        require_once "src/views/admin/dashboard_view.php";
+                    }
+                }else{
+                    require_once "src/views/connection/login_view.php";
+                }
+            }else{
+                require_once "src/views/connection/login_view.php";
+            }
+
+        }
+    }    
+    public function home(){
+        include_once  "src/views/home/home_view.php";
     }
 }
 ?>
