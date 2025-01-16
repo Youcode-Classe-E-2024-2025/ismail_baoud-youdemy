a589d439ccd57b62e22b90409d56b1bd355f82cd
ismail baoud <ibaoud46@gmail.com>
2025-01-16 22:26:49 +0100

    ajouter les fonctionnalites de suppression et modefecation des cours et obtenu la list des cours

--- src/modeles/courseModel.php
+++ src/modeles/courseModel.php
@@ -12,12 +12,11 @@ class courseModel {
         $this->db = DatabaseConnection::connect();
     }
 
-    public function courseAdd(course $obj){
+    public function courseAdd(course $obj,$id){
         try{
-session_start();
             $query = "INSERT into courses (title,description,content,categoryId,teacherID) values (?,?,?,?,?)";
             $stmt = $this->db->prepare($query);
-            $stmt->execute([$obj->__get("title"),$obj->__get("description"),$obj->__get("content"),$obj->__get("categoryId"),$_SESSION["teacherID"]]);
+            $stmt->execute([$obj->__get("title"),$obj->__get("description"),$obj->__get("content"),$obj->__get("categoryId"),$id]);
             return $this->db->lastInsertId();
         }catch(exeption $e){
             return false;
@@ -35,24 +34,32 @@ session_start();
         }
     }
 
-    public function courseDelete(){
-
+    public function courseDelete($id){
+        $query = "UPDATE courses set status = 'deactivate' where courseID =$id";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
     }
 
 
-    public function courseUpdate(){
-
+    public function courseUpdate($id,$title,$desc){
+     
+        $query = "UPDATE courses SET title = :title , description = :desc where courseID =$id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindparam("title", $title);
+        $stmt->bindparam("desc", $desc);
+        $stmt->execute();
     }
 
-    public function courseList(){
-        $query = "SELECT * FROM courses";
+
+    public function courseList($id){
+        $query = "SELECT * FROM courses where status = 'active' and teacherID = $id";
         $stmt = $this->db->query($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function totalCourses(){
-
+        
     }
 
     public function courseByCategory(){
