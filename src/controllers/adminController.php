56f475b6e28a65b6e7f0c0891758658af60dc02d
ismail baoud <ibaoud46@gmail.com>
2025-01-16 13:15:32 +0100

    ajouter a le controlleur d'admin et le fonctionnqliter d'ajoute des category et ajout des tags

--- src/controllers/adminController.php
+++ src/controllers/adminController.php
@@ -1,8 +1,34 @@
 <?php
+
 namespace src\controllers;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+use src\classes\category;
+use src\modeles\categoryModel;
+use src\classes\tag;
+use src\modeles\tagModel;
 class adminController{
     public function dashboard(){
         include_once  "src/views/admin/dashboard_view.php";
     }
+    public function addCategory(){
+        $category = $_POST["category"];
+        $obj = new category($category);
+        $objet = new categoryModel();
+        $res = $objet->categoryInsert($obj);
+        echo $res;
+    }
+    public function addTag(){
+        $tag = $_POST["tags"];
+        $tags = explode(",", $tag);
+        foreach($tags as $tag){
+            $obj = new tag($tag);
+            $objet = new tagModel();
+            $res = $objet->tagInsert($obj);
+            echo $res;
+        }
+    }
+    
+    
 }
 ?>
