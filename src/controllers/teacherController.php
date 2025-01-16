7834fd28bc9521460c9aabd09473464d0ae476a2
ismail baoud <ibaoud46@gmail.com>
2025-01-16 13:18:14 +0100

    afficher la list the tags dans le selection dans la page d'enseignant

--- src/controllers/teacherController.php
+++ src/controllers/teacherController.php
@@ -1,8 +1,18 @@
-
 <?php
+
+namespace src\controllers;
+use src\modeles\categoryModel;
+use src\modeles\tagModel;
 class teacherController{
+    
     public function dashboard(){
+        $objet = new tagModel();
+        $tags = $objet->tagList();
         include_once  "src/views/teacher/dashboard_view.php";
+    }
+    public function panding(){
+        include_once  "src/views/teacher/panding_view.php";
+
     }
 }
 ?>
