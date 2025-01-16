19b0f645e259e94116f1e394649d28f65eb106c8
ismail baoud <ibaoud46@gmail.com>
2025-01-16 17:27:28 +0100

    obtenu tou les course dans la base donnee in teacherController

--- src/controllers/teacherController.php
+++ src/controllers/teacherController.php
@@ -2,12 +2,17 @@
 
 namespace src\controllers;
 use src\modeles\categoryModel;
+use src\modeles\courseModel;
 use src\modeles\tagModel;
 class teacherController{
     
     public function dashboard(){
         $objet = new tagModel();
         $tags = $objet->tagList();
+        $obj = new courseModel();
+        $results = $obj->courseList();
+
+
         include_once  "src/views/teacher/dashboard_view.php";
     }
     public function panding(){
