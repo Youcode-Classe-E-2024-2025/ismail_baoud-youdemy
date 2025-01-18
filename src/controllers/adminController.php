41e511dadc654e82798b83a7601d291a7cee06ba
ismail baoud <ibaoud46@gmail.com>
2025-01-18 22:52:23 +0100

    cree indstence du classe teacher et returner troi tableaux des donnee depandans a status d'enseignant

--- src/controllers/adminController.php
+++ src/controllers/adminController.php
@@ -7,19 +7,24 @@ use src\classes\category;
 use src\modeles\categoryModel;
 use src\classes\tag;
 use src\modeles\tagModel;
+use src\modeles\teacherModel;
 class adminController{
     public function dashboard(){
+        $teacher = new teacherModel();
+        $panding = $teacher->pandingTeachers('On hold');
+        $active = $teacher->pandingTeachers('active');
+        $deactivate = $teacher->pandingTeachers('deactivate');
         include_once  "src/views/admin/dashboard_view.php";
     }
     public function addCategory(){
-        $category = $_POST["category"];
+        $category = htmlspecialchars(trim($_POST["category"]));
         $obj = new category($category);
         $objet = new categoryModel();
         $res = $objet->categoryInsert($obj);
         echo $res;
     }
     public function addTag(){
-        $tag = $_POST["tags"];
+        $tag = htmlspecialchars(trim($_POST["tags"]));
         $tags = explode(",", $tag);
         foreach($tags as $tag){
             $obj = new tag($tag);
