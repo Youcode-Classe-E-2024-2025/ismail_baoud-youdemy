918d04298feb4ad0ad9a3e282bb04b34efc14773
ismail baoud <ibaoud46@gmail.com>
2025-01-18 13:04:16 +0100

    ajouter le un fonction pour consulter tou les cours dans la page d'etudiant

--- src/controllers/studentController.php
+++ src/controllers/studentController.php
@@ -1,7 +1,14 @@
 <?php
 namespace src\controllers;
+use src\modeles\courseModel;
+use src\modeles\tagModel;
 class studentController{
     public function dashboard(){
+        $objet = new tagModel();
+        $tags = $objet->tagList();
+        $obj = new courseModel();
+        $id = $_SESSION["teacherID"];
+        $results = $obj->courseList($id);
         include_once  "src/views/student/dashboard_view.php";
     }
     public function mycourses(){
