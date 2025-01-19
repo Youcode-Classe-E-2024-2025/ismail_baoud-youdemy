61d5da3ab2b9533cedf4d9e7bd562c90df20ff01
ismail baoud <ibaoud46@gmail.com>
2025-01-19 23:48:28 +0100

    ajouter la fonctionnalites de controlleur pour pagination et ausse pour recherche par mot cle

--- src/controllers/homeController.php
+++ src/controllers/homeController.php
@@ -5,8 +5,38 @@ use src\modeles\tagModel;
 class homeController{
     
     public function home(){
-        $course = new courseModel();
-        $courses = $course->courseList();
+        $itemPerPage = 12;
+
+   
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1 ;
+
+        
+        
+        $model = new courseModel();
+        $count = $model->totalCourses();
+        
+        $totalPages =ceil($count/$itemPerPage) ;
+        
+        if($page <= 0 || $page >$totalPages){
+            
+            $page=1;
+        }
+        
+        
+        $offset = $itemPerPage * ($page-1);
+        $articleModel = new courseModel();
+
+        $motCle = isset($_GET['mot_cle']) ? $_GET['mot_cle'] : '';
+        if ($motCle) {
+            $courses = $articleModel->recherche($motCle,$itemPerPage ,$offset);
+        } else {
+            $courses = $articleModel->getSlice($itemPerPage ,$offset);
+        }
+        // $model = new courseModel();
+        // $courses = $model->getSlice($itemPerPage,(int)$offset);
+        // dd($courses);
+        // $course = new courseModel();
+        // $courses = $course->courseList();
         $tag = new tagModel();
         include_once "src/views/home/home_view.php";
     }
