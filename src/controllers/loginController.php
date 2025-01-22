123bb516949922255aba341f7fe066f820ec00be
ismail baoud <ibaoud46@gmail.com>
2025-01-22 13:50:20 +0100

    ajouter la page de 404 pour rederecter l'utilisateur quand le status est deactivate

--- src/controllers/loginController.php
+++ src/controllers/loginController.php
@@ -13,45 +13,53 @@ class loginController{
         $status;
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $email = htmlspecialchars($_POST['email']);
-            $password = trim($_POST['password']);
-            if(!isset($email) && !isset($password)){
-                $err = "data is empty";
-                return;
-            }
-            $obg = new userModel();
-            $result = $obg->login($email, $password);
-            if($result){
-                
-                if(password_verify( $password, $result['password'] )){
-                    if($result["role"] == "student"){
-                        $_SESSION["role"] = "student";
-                        $_SESSION["studentid"] = $result["userID"];
-                        header("location: /student/dashboard");
-                    }
-                    elseif($result["role"] == "teacher"){
-                        if($result["status"] == "On hold"){
-                            $_SESSION["role"] = "teacher";
-                            $_SESSION["status"] = "On hold";
-                            header("location: /teacher/dashboard/panding");
-                            
-                        }elseif($result["status"] == "active"){
-                            $_SESSION["teacherID"] = $result["userID"];
-                            $_SESSION["role"] = "teacher";
-                            $_SESSION["status"] = "active";
-                            header("location: /teacher/dashboard");
+            
+
+                $password = trim($_POST['password']);
+                if(!isset($email) && !isset($password)){
+                    $err = "data is empty";
+                    return;
+                }
+                $obg = new userModel();
+                $result = $obg->login($email, $password);
+                if($result){
+                    
+                    if(password_verify( $password, $result['password'] )){
+                        if($result["role"] == "student"){
+                            $_SESSION["role"] = "student";
+                            $_SESSION["studentid"] = $result["userID"];
+                            header("location: /student/mycourses");
+                        }
+                        elseif($result["role"] == "teacher"){
+                            if($result["status"] == "On hold"){
+                                $_SESSION["role"] = "panding";
+                                $_SESSION["status"] = "On hold";
+                                header("location: /teacher/dashboard/panding");
+                                
+                            }elseif($result["status"] == "active"){
+                                $_SESSION["teacherID"] = $result["userID"];
+                                $_SESSION["role"] = "teacher";
+                                $_SESSION["status"] = "active";
+                                header("location: /teacher/dashboard");
+                            }else{
+                                header("location: ../error/404.php");
+                            }
+                        }else{
+                            $_SESSION["role"] = "admin";
+                            header('location: /admin/dashboard');
                         }
                     }else{
-                        $_SESSION["role"] = "admin";
-                        header('location: /admin/dashboard');
+                        require_once "src/views/connection/login_view.php";
                     }
                 }else{
                     require_once "src/views/connection/login_view.php";
                 }
             }else{
                 require_once "src/views/connection/login_view.php";
+
             }
 
-        }
+        
     }    
     public function login_view(){
         include_once  "src/views/connection/login_view.php";
