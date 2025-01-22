5588e650b3fc8eed838c13bcdb31ea0c42e18a06
ismail baoud <ibaoud46@gmail.com>
2025-01-22 13:46:58 +0100

    strecturer le code

--- src/controllers/adminController.php
+++ src/controllers/adminController.php
@@ -5,20 +5,41 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 use src\classes\category;
 use src\modeles\categoryModel;
+use src\modeles\userModel;
 use src\classes\tag;
 use src\modeles\tagModel;
 use src\modeles\teacherModel;
 use src\modeles\courseModel;
 class adminController{
     public function dashboard(){
+        $teach = new userModel();
         $teacher = new teacherModel();
-        $allUsers = $teacher->allTeachers();
+        $course = new courseModel();
+        $cate = new categoryModel();
+        $tag = new tagModel();
+        $course = new userModel();
+        $top = new teacherModel();
+        $tope = new courseModel();
+        //all users
+        $allUsers = $teach->allUsers();
+        //all teachers dependence for status
         $panding = $teacher->pandingTeachers('On hold');
         $active = $teacher->pandingTeachers('active');
         $deactivate = $teacher->pandingTeachers('deactivate');
-        $course = new courseModel();
+        //all courses
         $courses = $course->courseList();
-
+        $totalCourses = $course->totalCourses();
+        //total categories
+        $totalCategory = $cate->totalCategory();
+        //total tags
+        $totalTags = $tag->totalTags();
+        //total users and panding
+        $usersN = $course->CountallUsers();
+        $pandingN = $course->CountAllTeachers();
+        //top three teachers
+        $topteachers = $top->topTeachers();
+        //top course
+        $topCourse = $tope->topCourse();
 
         include_once  "src/views/admin/dashboard_view.php";
     }
@@ -27,7 +48,7 @@ class adminController{
         $obj = new category($category);
         $objet = new categoryModel();
         $res = $objet->categoryInsert($obj);
-        echo $res;
+        header('location: /admin/dashboard');
     }
     public function addTag(){
         $tag = htmlspecialchars(trim($_POST["tags"]));
@@ -36,7 +57,8 @@ class adminController{
             $obj = new tag($tag);
             $objet = new tagModel();
             $res = $objet->tagInsert($obj);
-            echo $res;
+            header('location: /admin/dashboard');
+
         }
     }
     public function changeUserStatus(){
