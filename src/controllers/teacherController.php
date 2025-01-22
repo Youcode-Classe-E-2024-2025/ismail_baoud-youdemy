6846e00dd8c24535ba1d7a3b061eac59c0786cb4
ismail baoud <ibaoud46@gmail.com>
2025-01-22 13:53:41 +0100

    obtenu tous le count des course

--- src/controllers/teacherController.php
+++ src/controllers/teacherController.php
@@ -3,23 +3,26 @@
 namespace src\controllers;
 use src\modeles\categoryModel;
 use src\modeles\courseModel;
+use src\modeles\enrollementModel;
 use src\modeles\tagModel;
 class teacherController{
     
     public function dashboard(){
-        $countCourses = 0;
+      
+        $enr = new enrollementModel();
+        
         $objet = new tagModel();
         $tags = $objet->tagList();
         $obj = new courseModel();
         $id = $_SESSION["teacherID"];
         $results = $obj->courseList($id);
-        foreach($results as $result){
-            $countCourses++;
-        }
+        $countCourses = $obj->totalCourses(); 
+        
 
 
         include_once  "src/views/teacher/dashboard_view.php";
     }
+
     public function panding(){
         include_once  "src/views/teacher/panding_view.php";
     }
